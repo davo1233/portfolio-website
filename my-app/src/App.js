b5b0818bd6db8ab6807b2de1b1db073d696c8dc0
Spenser Tan <51882646+davo1233@users.noreply.github.com>
2023-12-24 23:20:53 +1100

    added menu that changes when it becomes mobile view

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,49 +1,33 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  VStack,
-  theme,
-  Flex,
-  Heading,
-  Container,
-  Stack,
-  Box,
-
-} from '@chakra-ui/react';
+import { ChakraProvider, VStack, theme, Grid, Box } from '@chakra-ui/react';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Stack>
+      <Grid
+        templateColumns="1fr" // Single column layout
+        templateRows="auto 1fr" // First row auto-sized for Navbar, second row takes the rest of the space
+        height="100vh" // Full height
+      >
+        {/* Navbar */}
+        <Box>
+          <Navbar />
+        </Box>
 
-        <Flex w="100%" direction = "column" h = "100%">
-			
-            
-
-			<Box w = "100%">
-				<Navbar />
-			</Box>
-
-			<Box flex = "1">
-				<Routes>
-					<Route exact path="/" element={<Home />} />
-					<Route exact path="/about" element={<About />} />
-					<Route exact path="/contact" element={<Contact />} />
-				</Routes>
-			</Box>
-			
-
-             
-            
-
-        </Flex>
-      </Stack>
-      
+        {/* Main Content */}
+        <Box overflowY="auto" padding="4">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+          </Routes>
+        </Box>
+      </Grid>
     </ChakraProvider>
   );
 }
--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,50 +1,87 @@
-import {  IconButton, useMediaQuery, Link, Box, Flex, StackDivider, Text, HStack, VStack, Container } from "@chakra-ui/react";
-import ColorModeSwitcher from "./ColorModeSwitcher";
-import { BrowserRouter as Router, Link as RouteLink, Routes, Route } from "react-router-dom";
+import {
+    IconButton,
+    useMediaQuery,
+    Link,
+    Box,
+    Flex,
+    StackDivider,
+    Text,
+    HStack,
+    VStack,
+    Button,
+    Collapse,
+  } from "@chakra-ui/react";
+  import ColorModeSwitcher from "./ColorModeSwitcher";
+  import { BrowserRouter as Router, Link as RouteLink, Routes, Route } from "react-router-dom";
+  import { useBreakpointValue } from "@chakra-ui/react";
+  import React, { useState } from "react";
+  import { Logo } from "./Logo";
+  import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+  
+  /*
+  the navbar which is displayed in the top bar that allows the user to navigate to different parts of the site
+  and also contains the links to my socials
+  */
+  const Navbar = () => {
 
-import React from "react";
-import { Logo } from "./Logo";
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
+    const [isOpen, setIsOpen] = useState(false);
+    const isMobile = useBreakpointValue({ base: true, md: false }, { ssr: false });  
 
-import { FaInstagram,FaLinkedin, FaGithub } from "react-icons/fa";
-
-/*
-the navbar which is displayed in the top bar that allows the user to navigate to different parts of the site
-and also contains the links to my socials
-*/
-function Navbar() {
-    
-    const NavLink = ( {to,text} ) => (
-        <Link as = {RouteLink} to = {to}>
-          <Text fontSize="xl">{text}</Text>
-        </Link>
+  
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
+  
+    const NavLink = ({ to, text }) => (
+      <Link as={RouteLink} to={to}>
+        <Text fontSize="xl">{text}</Text>
+      </Link>
     );
+  
     return (
-        <Flex justify="space-between" align="center" py={4}>
-            <Box>
-               <Logo></Logo>
+      <Flex justify="space-between" align="center" py={4}>
+        <Box>
+          <Logo></Logo>
+        </Box>
+        {isMobile ? (
+          <Box  color="white">
+            <Button onClick={toggleMenu}>Menu</Button>
+  
+            <Collapse in={isOpen}>
+              <VStack align="start" spacing={2} mt={2}>
+                <NavLink to="/" text="Home" />
+                <NavLink to="/about" text="About" />
+                <NavLink to="/contact" text="Contact" />
+                <NavLink to="/hobbies" text="Hobbies" />
+              </VStack>
+            </Collapse>
+          </Box>
+        ) : (
+          <Box>
+            <HStack spacing={3} divider={<StackDivider />} ml={20}>
+              <NavLink to="/" text="Home" />
+              <NavLink to="/about" text="About Me" />
+              <NavLink to="/contact" text="Contact" />
+              <NavLink to="/hobbies" text="Hobbies" />
+            </HStack>
+          </Box>
+        )}
+  
+        <Box>
+            <Link href="https://www.instagram.com/tenserspan/" isExternal>
+            <IconButton icon={<FaInstagram />} ml={2} p={2} />
+            </Link>
+            <Link href="https://www.linkedin.com/in/spenser-tan-25372b169/" isExternal>
+            <IconButton icon={<FaLinkedin />} ml={2} p={2} />
+            </Link>
+            <Link href="https://github.com/davo1233" isExternal>
+            <IconButton icon={<FaGithub />} ml={2} p={2} />
+            </Link>
+            <ColorModeSwitcher ml={2} p={2} />
             </Box>
-            <Box>
-                <HStack spacing = {3} divider = {<StackDivider/>} ml={20}>             
-                        <NavLink to="/" text="Home" ml={2} />
-                        <NavLink to="/about" text="About Me" ml={2} /> 
-                        <NavLink to="/contact" text="Contact" ml={2} />
-                        <NavLink to="/hobbies" text="Hobbies" ml={2} />
-                </HStack> 
-            </Box>
-            <Box>
-                <IconButton icon= {<FaInstagram/>} ml = {2} p ={2}></IconButton>
-                <IconButton icon = {<FaLinkedin/>} ml = {2} p ={2}></IconButton>
-                <IconButton icon = {<FaGithub/>} ml = {2} p ={2}></IconButton>
-                <ColorModeSwitcher ml = {2} p ={2}></ColorModeSwitcher>
-            </Box>
-             
-        </Flex>
-           
-        
+      </Flex>
     );
-}
-
-export default Navbar;
+  };
+  
+  export default Navbar;
+  
