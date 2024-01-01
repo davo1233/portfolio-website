ce28bfb01e183893822df5c63aa0ba1f0ea51001
Spenser Tan <51882646+davo1233@users.noreply.github.com>
2024-01-01 20:46:53 +1100

    create slide pt data

---
+++ my-app/src/imagesData/SlidePTData.js
@@ -0,0 +1,20 @@
+
+export const SlidePTData = [
+    {
+      image:
+        process.env.PUBLIC_URL + '/images/mitchells_pass.jpg'
+    },
+    {
+      image:
+      process.env.PUBLIC_URL + '/images/field.jpg'
+    },
+    {
+      image:
+      process.env.PUBLIC_URL + '/images/woywoy.jpg'
+    },
+    {
+      image:
+      process.env.PUBLIC_URL + '/images/beach.jpg'
+    },
+    
+  ];
